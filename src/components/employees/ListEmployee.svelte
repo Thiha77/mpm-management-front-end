7f02d3c373784d913467743814438d4758c5a106
MayPhueKyawSoe <mayphuekyawsoe123@gmail.com>
2020-02-26 16:23:13 +0630

    employee

---
+++ src/components/employees/ListEmployee.svelte
@@ -0,0 +1,93 @@
+<script>
+export let employees;
+import ApiPost from '../../util/api.js';
+// import  CreatedEmp from '../../components/employees/CreateEmployee.svelte';
+import { createEventDispatcher } from "svelte";
+
+let deletebtn = id => {
+       if(confirm("Are you sure?")){
+            const url= "http://localhost:5000/employees/delete";
+            const body ={
+                id:id
+         }
+         ApiPost(url,body);
+         employees = employees.filter(a => a.id !== id);
+       };                
+    };
+     function createForm(){
+         window.location="employee/create";
+     }
+
+
+</script>
+<style>
+table.table .avatar {
+    border-radius: 50%;
+    vertical-align: middle;
+    margin-right: 10px;
+	}
+.employee-photo{
+    width:70px;
+    height:70px;
+}
+table.table td a {
+    font-weight: bold;
+    color: #566787;
+    display: inline-block;
+    text-decoration: none;
+}
+ table.table tr th, table.table tr td {
+    border-color: #e9e9e9;
+    padding: 12px 15px;
+    vertical-align: middle;
+}
+table.table td a {
+    font-weight: bold;
+    color: #566787;
+    display: inline-block;
+    text-decoration: none;
+}
+</style>
+<div class="container">
+        <div class="table-wrapper">
+            <div class="table-title">
+                <div class="row">
+                    <div class="col-sm-8">
+						<h2>Employee <b>Management</b></h2>
+					</div>
+					<div class="col-sm-4">
+						<button  class="btn btn-primary float-right" on:click={createForm}>Add New Employee</button>					
+					</div>
+                </div>
+            </div>
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Photo</th>						
+						<th>Phone</th>
+                        <th>Official Email</th>
+						<th>Address</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {#if employees.length ===0}
+                    <tr>No  Record Found!</tr>
+                 {:else}
+                 {#each employees as employee}   
+                    <tr>
+                        <td><a href="test"><img src="great-success.png"  class="avatar img-thumbnail employee-photo" alt="background image"/>{employee.name}</td>
+                        <td>{employee.phoneNo}</td>
+                        <td>{employee.officialEmail}</td>
+                        <td>{employee.address}</td>
+						<td>
+						    <button class="btn btn-info">Edit</button>
+                            <button class="btn btn-danger" on:click={deletebtn(employee.id)}>Delete</button>
+                        </td>
+                        </tr>
+                     {/each} 
+                    {/if}   
+                </tbody>
+            </table>
+        </div>
+    </div>  
---
+++ src/routes/employee/create/index.svelte
@@ -0,0 +1,38 @@
+<script>
+ import Api from '../../../components/util/Api.svelte';
+ import ApiPost from '../../../util/api.js';
+import EmpCreate from '../../../components/employees/CreateEmployee.svelte';
+
+        // let empData = {
+        //     name: "",
+        //     alias: "",
+        //     phoneNo: "",
+        //     nrcNo: "",
+        //     personalEmail: "",
+        //     officialEmail: "",
+        //     township: "",
+        //     city: "",
+        //     address: "",
+        //     postalCode: "",
+        //     dob: "",
+        //     gender: "",
+        //     position: "",
+        //     basicSalary: "",
+        //     nationality: "",
+        //     race: "",
+        //     maritalStatus: "",
+        //     employeeStatus: ""
+        // };
+
+        const CreateData = (event) => {
+            let url = "http://localhost:5000/employees/create";
+            console.log(event.detail.emp);
+            //  ApiPost(url,)
+            //ApiPost(url,empData);
+        };        
+</script>
+<!-- <Api></Api> -->
+<!-- <EmpCreate on:create={CreateData} bind:name={empData.name} bind:alias={empData.alias} bind:phoneNo={empData.phoneNo} bind:nrcNo={empData.nrcNo} bind:personalEmail={empData.personalEmail} bind:officialEmail={empData.officialEmail} bind:township={empData.township} bind:city={empData.city} bind:address={empData.address} bind:postalCode={empData.postalCode} 
+    bind:dob={empData.dob} bind:gender={empData.gender} bind:position={empData.position} bind:basicSalary={empData.basicSalary} 
+    bind:nationality={empData.nationality} bind:race={empData.race} bind:maritalStatus={empData.maritalStatus} bind:employeeStatus={empData.employeeStatus}></EmpCreate> -->
+<EmpCreate on:create={CreateData} ></EmpCreate>

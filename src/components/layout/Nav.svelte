a6e2978fe238254697625f230a32af3886ce7861
mayzin_mmp <mayzin@management-partners.co.jp>
2020-02-27 14:37:17 +0630

    sidebar

--- src/components/layout/Nav.svelte
+++ src/components/layout/Nav.svelte
@@ -1,9 +1,10 @@
 <script>
-	export let segment;
+	// export let segment;
+	import Sidebar from '/mpm/Sidebar.svelte';
 </script>
 
 <style>
-	nav {
+	/* nav {
 		border-bottom: 1px solid rgba(255,62,0,0.1);
 		font-weight: 300;
 		padding: 0 1em;
@@ -12,10 +13,10 @@
 	ul {
 		margin: 0;
 		padding: 0;
-	}
+	} */
 
 	/* clearfix */
-	ul::after {
+	/* ul::after {
 		content: '';
 		display: block;
 		clear: both;
@@ -45,17 +46,15 @@
 		text-decoration: none;
 		padding: 1em 0.5em;
 		display: block;
-	}
+	} */
 </style>
 
-<nav>
+<!-- <nav>
 	<ul>
 		<li><a class:selected='{segment === undefined}' href='.'>home</a></li>
 		<li><a class:selected='{segment === "about"}' href='about'>about</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
 		<li><a rel=prefetch class:selected='{segment === "blog"}' href='blog'>blog</a></li>
 		<li><a href='admin' class:selected={segment === 'admin'}>Admin</a></li>
 	</ul>
-</nav>
+</nav> -->
+<Sidebar></Sidebar>
---
+++ src/components/layout/mpm/Sidebar.svelte
@@ -0,0 +1,51 @@
+<script>
+	// export let segment;
+</script>
+<nav id="sidebar">
+    <div class="sidebar-header">
+        <h3>Bootstrap Sidebar</h3>
+        <strong>BS</strong>
+    </div>
+
+    <ul class="list-unstyled components">
+        <li class="active">
+            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                <i class="fas fa-home"></i>
+                Home
+            </a>
+            <ul class="collapse list-unstyled" id="homeSubmenu">
+                <li>
+                    <a href="#1">Home 1</a>
+                </li>
+                <li>
+                    <a href="#1">Home 2</a>
+                </li>
+                <li>
+                    <a href="#1">Home 3</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="#1">
+                <i class="fas fa-briefcase"></i>
+                About
+            </a>
+            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                <i class="fas fa-copy"></i>
+                Pages
+            </a>
+            <ul class="collapse list-unstyled" id="pageSubmenu">
+                <li>
+                    <a href="#1">Page 1</a>
+                </li>
+                <li>
+                    <a href="#1">Page 2</a>
+                </li>
+                <li>
+                    <a href="#1">Page 3</a>
+                </li>
+            </ul>
+        </li>
+    </ul>
+
+</nav>

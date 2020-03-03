2023b9e3d219c26f2149104b86cf66ddc1e6294c
mayzin_mmp <mayzin@management-partners.co.jp>
2020-03-03 16:24:01 +0630

    sidebar & nav

--- src/components/layout/mpm/Sidebar.svelte
+++ src/components/layout/mpm/Sidebar.svelte
@@ -8,7 +8,7 @@ import { nav } from '../../../store';
 {#if $nav.showSideBar}
 <nav id="sidebar">
     <div class="sidebar-header">
-        <h3>Bootstrap Sidebar</h3>
+        <h3>MPM Management System</h3>
         <strong>BS</strong>
     </div>
 
@@ -73,13 +73,13 @@ import { nav } from '../../../store';
         </li>
     </ul>
 
-    <ul class="list-unstyled CTAs">
+    <!-- <ul class="list-unstyled CTAs">
         <li>
             <a href="https://bootstrapious.com/tutorial/files/sidebar.zip" class="download">Download source</a>
         </li>
         <li>
             <a href="https://bootstrapious.com/p/bootstrap-sidebar" class="article">Back to article</a>
         </li>
-    </ul>
+    </ul> -->
 </nav>
 {/if}

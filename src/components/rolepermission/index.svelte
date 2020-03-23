99fe0034a8fd7a491f5ba6a5098199b43db9c68c
phyuzin-95 <phyuzin2095@gmail.com>
2020-03-23 13:22:42 +0630

    RolePermission_Group

--- src/components/rolepermission/index.svelte
+++ src/components/rolepermission/index.svelte
@@ -1,6 +1,7 @@
 <script>
     import { editRolePermission } from '../../stores/rolepermission/store.js';
     import RolePermissionSearch from './search.svelte';
+    import Grouper from './grouper.svelte';
     import { createEventDispatcher } from 'svelte';
     const dispatch = createEventDispatcher();
     export let rolePermissions;
@@ -19,7 +20,12 @@
     const searchRolePermission = (event) => {
         let search = event.detail.search;
         dispatch('searchRolePermissionData',{search});
-    };
+    }
+
+    function groupForItem(rolePermission)
+    {
+		return rolePermission.Role['name']
+	}
 </script>
 <style>
 	
@@ -36,34 +42,28 @@
                                 <div class="col-sm-6"><h3 class="float-left">Role Permission List</h3></div>
                                 <div class="col-sm-6"><a class="btn btn-info float-right" href='rolepermission/create'><i class="fas fa-plus-circle"></i> Add New Role Permission</a></div>
                             </div>
-                        </div><!-- .table-title -->
-                        <table class="table table-hover">
-                            <thead>                                     
-                                <tr>
-                                    <th>Id</th>
-                                    <th>Role Name</th>
-                                    <th>Permission Name</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {#if rolePermissions}
-                                    {#each rolePermissions as rolePermission}
-                                        <tr>
-                                            <td>{rolePermission.id}</td>
-                                            <td>{rolePermission.Role['name']}</td>
-                                            <td>{rolePermission.Permission['name']}</td>
-                                            <td>
-                                                <!-- <a href="rolepermission/edit" on:click={edit(rolePermission)} class="btn btn-info">Edit</a> -->
-                                                <button on:click={destory(rolePermission.id)} class="btn btn-danger"><i class="far fa-trash-alt"></i></button>
-                                            </td>
-                                        </tr>
-                                    {/each}
-                                {/if}
-                            </tbody>
-                        </table><!-- .table-hover -->
-                    </div><!-- .table-wrapper -->
-                </div><!-- .table-responsive-sm -->
+                        </div>
+                        <Grouper rolePermissions={rolePermissions} groupForItem={groupForItem} let:group let:rolePermission>
+                            <div slot='group'>
+                                <h2>{group}</h2>
+                            </div>
+                            <div slot='rolePermission'>
+                                {rolePermission}
+                            </div>
+                        </Grouper>
+                    </div>
+                </div>
+                <!-- <div class="card" style="margin-top:20px;">
+                    <div class="card-header" style="background:#0689CE;">
+                         <div class="row">
+                            <div class="col-sm-6"><h3 class="float-left" style="color:white;">Role Permission List</h3></div>
+                            <div class="col-sm-6"><a class="btn btn-info float-right" href='rolepermission/create'><i class="fas fa-plus-circle"></i> Add New Role Permission</a></div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                       
+                    </div>
+                </div> -->
             </div>
         </div>
     </div>

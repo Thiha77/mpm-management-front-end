418226f85644e75864643b6c429cb0152a983812
thiha77 <aungthiha91027@gmail.com>
2020-03-24 09:39:55 +0630

    notice all forms add multi lan

--- src/components/notice/edit.svelte
+++ src/components/notice/edit.svelte
@@ -6,32 +6,33 @@
         dispatch('save', { notice: notice})
     }
     export let notice;
+    export let fields;
 </script>
 <div class="container">
     <div class="row">
         <div class="col mx-auto">
             <div class="card card-mpm1">
                 <div class="card-heading bg-blue-mpm p-3">
-                    <h3 class="text-white text-uppercase text-center">Notice Update Form</h3>
+                    <h3 class="text-white text-uppercase text-center">{fields.form.editTitle}</h3>
                 </div>
                 <div class="card-body p-5">
                     <div>
                         <div class="form-group">
-                            <label for="title">Title:</label>
-                            <input type="text" bind:value={notice.title} class="form-control" placeholder="Enter title" />
+                            <label for="title">{fields.notice.title}:</label>
+                            <input type="text" bind:value={notice.title} class="form-control" placeholder={fields.placeholder.enterTitle} />
                         </div>
                         <div class="form-group">
-                            <label for="desc">Description:</label>
-                            <textarea bind:value={notice.description} class="form-control" placeholder="Enter description" ></textarea>
+                            <label for="desc">{fields.notice.description}:</label>
+                            <textarea bind:value={notice.description} class="form-control" placeholder={fields.placeholder.enterDescription} ></textarea>
                         </div>
                         <div class="form-group">
-                            <label for="sum">Summary:</label>
-                            <textarea bind:value={notice.summary} class="form-control" placeholder="Enter summary" ></textarea>
+                            <label for="sum">{fields.notice.summary}:</label>
+                            <textarea bind:value={notice.summary} class="form-control" placeholder={fields.placeholder.enterSummary} ></textarea>
                         </div>
                     </div>
                     <div class="mt-4">
-                        <button type="button" on:click={saveNotice} class="btn btn-blue">Save</button>
-                        <a href="notice" class="btn btn-outline-orange">Cancel</a>       
+                        <button type="button" on:click={saveNotice} class="btn btn-blue">{fields.form.save}</button>
+                        <a href="notice" class="btn btn-outline-orange">{fields.form.cancel}</a>       
                     </div>
                 </div><!-- .card-body -->
             </div><!-- .card -->
